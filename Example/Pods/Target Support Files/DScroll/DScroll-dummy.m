2adc59cec933889ecd26cc58a3c37029ee088696
Nervana Adel <nervana.adel@adkatech.com>
2020-04-23 23:29:33 +0400

    Intial Commit

---
+++ Example/Pods/Target Support Files/DScroll/DScroll-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DScroll : NSObject
+@end
+@implementation PodsDummy_DScroll
+@end
---
+++ Example/Pods/Target Support Files/Pods-DScroll_Example/Pods-DScroll_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DScroll_Example : NSObject
+@end
+@implementation PodsDummy_Pods_DScroll_Example
+@end
